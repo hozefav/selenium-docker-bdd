0ee1aa73e39d63e1bd4086018a21f53893111788
hozefav <hozefav@gmail.com>
2021-01-08 19:00:14 +1100

    healthcheck file resending

--- ___healthcheck.sh
+++ ___healthcheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Environment Variables
+# Environment Variabless
 # HUB_HOST
 # BROWSER
 # MODULE
@@ -12,4 +12,7 @@ do
 done
 
 # start the java command
-java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* -DHUB_HOST=$HUB_HOST -DBROWSER=$BROWSER org.testng.TestNG $MODULE
+java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/testng-7.3.0:libs/* \
+    -DHUB_HOST=$HUB_HOST \
+    -DBROWSER=$BROWSER \
+    org.testng.TestNG $MODULE
